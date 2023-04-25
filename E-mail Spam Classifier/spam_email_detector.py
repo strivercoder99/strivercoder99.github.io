c515c5aedec7cfed344c4dada8f1c19884970c79
Aditya Raj Sinha <arsinha2001@gmail.com>
2023-04-25 20:22:22 +0530

    E-mail Spam Claasifier

---
+++ E-mail Spam Classifier/spam_email_detector.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+import streamlit as st
+import time
+st.title("Spam Message Classifier")
+#st.subheader("Building an Email Spam Classifier using Machine Learning")
+spam = pd.read_csv("spam_ham_dataset.csv")
+count_vectorizer = CountVectorizer()
+X_train = count_vectorizer.fit_transform(spam['text'].values.astype('U'))
+y_train = spam["label_num"]
+spam_detector = MultinomialNB().fit(X_train , y_train)
+msg = st.text_input("Enter a Text:")
+if st.button("predict"):
+    with st.spinner('Wait for It!'):
+        time.sleep(2)
+    X_test = count_vectorizer.transform([msg])
+    predicted_label = spam_detector.predict(X_test)
+    if predicted_label == 0:
+        st.error("This Mail is not Spam")
+
+    else:
+        st.error("This Message is Spam")
